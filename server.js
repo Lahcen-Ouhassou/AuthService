d4a1835fe9066b7b9cf37585a225048ddf12483c
lahceenn <lahcenouhassou18@gmail.com>
2025-11-10 14:57:57 +0100

    Server Upd

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.use("/api/auth", authRoutes);
 
-// test route
-app.get("/", (req, res) => {
-  res.send("AuthService API is running ✅");
-});
+const PORT = process.env.PORT || 5000;
 
-// connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ Connected to MongoDB");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
-    );
-  })
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
