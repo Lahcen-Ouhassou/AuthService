4b7883d9dc77ddb29987481084d378cbef047128
lahceenn <lahcenouhassou18@gmail.com>
2025-11-10 15:43:23 +0100

    Upd

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import {
+  registerUser,
+  loginUser,
+  authorizeUser,
+  requestPasswordReset,
+  verifyResetCode,
+  resetPassword,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authorizeUser);
+
+// Reset Password Steps
+router.post("/forgot-password", requestPasswordReset);
+router.post("/verify-code", verifyResetCode);
+router.post("/reset-password", resetPassword);
 
 export default router;
