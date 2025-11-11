c81d4d047ffa8a0c86c819ae1750b9ac0f6d8cdb
lahceenn <lahcenouhassou18@gmail.com>
2025-11-11 19:37:13 +0100

    UpdAuth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,12 +15,10 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-
-router.post("/request-reset", requestPasswordReset);
+router.post("/forgot-password", requestPasswordReset);
 router.post("/verify-code", verifyResetCode);
 router.post("/reset-password", resetPassword);
 
 router.get("/me", authMiddleware, me); // ✅ هادي اللي كانت كتخرج error
-
 
 export default router;
